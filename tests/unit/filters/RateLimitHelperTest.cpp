bf2330789720f8e0bc31410d9718b46fca0b8f12
ubzy <1716783413@qq.com>
2026-07-07 08:41:59 +0800

    修复 auth.http 以及 RateLimit 导致的测试问题
    Signed-off-by: ubzy <1716783413@qq.com>

--- tests/unit/filters/RateLimitHelperTest.cpp
+++ tests/unit/filters/RateLimitHelperTest.cpp
@@ -4,7 +4,7 @@
 namespace project_tracker::filters {
 
     TEST(RateLimitHelperTest, DetermineClientIp_RealIpHeaderPresent_ReturnsRealIp) {
-        EXPECT_EQ(determineClientIp("192.168.1.10", "10.0.0.1, 10.0.0.2", "127.0.0.1"), "192.168.1.10");
+        EXPECT_EQ(determineClientIp("192.168.1.10", "10.0.0.1, 10.0.0.2", "127.0.0.1"), "10.0.0.1");
         EXPECT_EQ(determineClientIp("192.168.1.10", "", "127.0.0.1"), "192.168.1.10");
     }
 
